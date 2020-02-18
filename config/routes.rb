a6cbf690835b93ecfc7bd1b4bac39b1ace988e7a
Thiago Menezes <thiagorsm@gmail.com>
2020-02-18 17:10:45 -0300

    Creates task app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # index list
+  get 'tasks', to: 'tasks#index'
+  # create new task
+  get 'tasks/new', to: 'tasks#new'
+  # task detail page
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # task create page
+  post 'tasks', to: 'tasks#create'
+  # show edit form
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'tasks_edit'
+  # update task
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
